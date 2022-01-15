4f0748a920141dbe363a2f292ab68e7218c5c33b
Romain <romain.chagnaud@etu.u-bordeaux.fr>
2022-01-15 23:51:32 +0100

    commit final

--- Projet_Web/Projet_Web/src/Form/RatingType.php
+++ Projet_Web/Projet_Web/src/Form/RatingType.php
@@ -6,7 +6,6 @@ use App\Entity\Rating;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\VarExporter\Internal\Values;
 
 class RatingType extends AbstractType
 {
